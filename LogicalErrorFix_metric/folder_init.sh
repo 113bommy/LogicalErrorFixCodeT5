0b00572e5be451a69a28cd597d300b9a28a59ed1
113bommy <113bommy@gmail.com>
2024-03-19 23:53:26 +0900

    Parse Error

---
+++ LogicalErrorFix_metric/folder_init.sh
@@ -0,0 +1,32 @@
+rm -r CodeT5_LineNumber_cpp_gold
+rm -r CodeT5_LineNumber_cpp_source
+rm -r CodeT5_LineNumber_cpp_output
+
+rm -r gold_compile_folder
+rm -r gold_test_case_error_log
+rm -r gold_test_result
+
+rm -r output_compile_folder
+rm -r output_test_case_error_log
+rm -r output_test_result
+
+rm -r source_compile_folder
+rm -r source_test_case_error_log
+rm -r source_test_result
+
+
+mkdir CodeT5_LineNumber_cpp_gold
+mkdir CodeT5_LineNumber_cpp_source
+mkdir CodeT5_LineNumber_cpp_output
+
+mkdir gold_compile_folder
+mkdir gold_test_case_error_log
+mkdir gold_test_result
+
+mkdir output_compile_folder
+mkdir output_test_case_error_log
+mkdir output_test_result
+
+mkdir source_compile_folder
+mkdir source_test_case_error_log
+mkdir source_test_result
